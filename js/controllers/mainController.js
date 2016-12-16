1bf31ca0b2b05619f6e984ef238ff5013682a17b
Camjr <cfontainef1@gmail.com>
2016-12-16 16:43:19 +0100

    v1

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,87 @@
-// MAIN CONTROLLER
-function mainController($scope) {
-    
+function mainController() {
+    this.logo = [{
+        href: 'https://fr.linkedin.com/in/camfontaine',
+        logo: 'linkedin',
+    }, {
+        href: 'https://github.com/CamJr',
+        logo: 'github',
+    }, {
+        href: 'https://www.youtube.com/channel/UCHEvYXLMVdUWmEM_4hrGm4A',
+        logo: 'youtube',
+    }, {
+        href: 'https://www.instagram.com/camjroff/',
+        logo: 'instagram',
+    }];
+
+    this.tableSkill = [{
+        skill: 'HTML/CSS',
+        number: 90
+    }, {
+        skill: 'JavaScript',
+        number: 80
+    }, {
+        skill: 'AngularJS',
+        number: 80
+    }, {
+        skill: 'MongoDB',
+        number: 75
+    }, {
+        skill: 'NodeJS',
+        number: 80
+    }, {
+        skill: 'Express',
+        number: 75
+    }, {
+        skill: 'Jquery',
+        number: 85
+    }, {
+        skill: 'Suite Adobe',
+        number: 95
+    }];
+
+    this.slide = [{
+        name: 'Movember Application',
+        img: 'moustache.png',
+        bio: 'Création dune application web mobile pour l’événement Movember.',
+        logo: 'github',
+        href: 'https://github.com/CamJr/Hackathon-Movember'
+    }, {
+      name: 'hopital la loupe',
+      img: 'chlaloupe.png',
+      bio: 'Création dun site vitrine administrable pour le centre Hospitalier de La Loupe.',
+      logo: 'github',
+      href: 'https://github.com/WildCodeSchool/laloupe-0916-ch-laloupe'
+    }, {
+        name: 'ComparateurAgricole.com',
+        img: 'biagri.png',
+        bio: 'Réalisation dun site web dachat et de vente de produits agricoles.',
+        logo: 'github',
+        href: 'https://github.com/Jipsian/Hackathon-Biagri'
+    }, {
+        name: 'Agrividéo',
+        img: 'agrivideo.png',
+        bio: 'Création dune plateforme de cours en ligne autour de lagriculture.',
+        logo: 'github',
+        href: 'https://github.com/Celine-Langlade/Agrivideo'
+    }];
+
+    this.photoDiapo = [{
+        photo: 'chateau.jpg',
+        name: 'chateau'
+    }, {
+        photo: 'cristal.jpg',
+        name: 'cristaux de neige'
+    }, {
+        photo: 'star.jpg',
+        name: 'photo étoiles'
+    }, {
+        photo: 'rue.jpg',
+        name: 'photo rue'
+    }, {
+        photo: 'leverdesoleil.jpg',
+        name: 'photo lever de soleil'
+    }, {
+        photo: 'coucherdesoleil.jpg',
+        name: 'photo coucher de soleil'
+    }];
 }
