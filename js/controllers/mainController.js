9bb8a049bcf50672d43c3bb4ee40843c7a81217d
Camjr <cfontainef1@gmail.com>
2017-04-22 18:52:39 +0200

    Ajout photo, correction de bug

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,193 +1,197 @@
   function mainController() {
-    this.icon = [{
-      href: 'https://fr.linkedin.com/in/camfontaine',
-      logo: 'linkedin',
-    }, {
-      href: 'https://github.com/CamJr',
-      logo: 'github',
-    }, {
-      href: 'https://github.com/CamJr',
-      logo: 'Instagram',
-    }];
+      this.icon = [{
+          href: 'https://fr.linkedin.com/in/camfontaine',
+          logo: 'linkedin',
+      }, {
+          href: 'https://github.com/CamJr',
+          logo: 'github',
+      }, {
+          href: 'https://github.com/CamJr',
+          logo: 'Instagram',
+      }];
 
-    this.logoCode = [{
-      logo: 'html',
-    }, {
-      logo: 'css',
-    }, {
-      logo: 'javascript',
-    }, {
-      logo: 'jquery',
-    }, {
-      logo: 'angular',
-    }, {
-      logo: 'nodejs',
-    }, {
-      logo: 'express',
-    }, {
-      logo: 'mongodb',
-    }, {
-      logo: 'electron',
-    }, {
-      logo: 'wordpress',
-    }, {
-      logo: 'materialize',
-    }, {
-      logo: 'bootstrap',
-    }];
+      this.logoCode = [{
+          logo: 'html',
+      }, {
+          logo: 'css',
+      }, {
+          logo: 'javascript',
+      }, {
+          logo: 'jquery',
+      }, {
+          logo: 'angular',
+      }, {
+          logo: 'nodejs',
+      }, {
+          logo: 'express',
+      }, {
+          logo: 'mongodb',
+      }, {
+          logo: 'electron',
+      }, {
+          logo: 'wordpress',
+      }, {
+          logo: 'materialize',
+      }, {
+          logo: 'bootstrap',
+      }];
 
-    this.logoDesign = [{
-      logo: 'photoshop',
-    }, {
-      logo: 'illustrator',
-    }, {
-      logo: 'premiere',
-    }, {
-      logo: 'adobeXd',
-    }, {
-      logo: 'gimp',
-    // }, {
-    //   logo: 'inkScape',
-    }];
+      this.logoDesign = [{
+          logo: 'photoshop',
+      }, {
+          logo: 'illustrator',
+      }, {
+          logo: 'premiere',
+      }, {
+          logo: 'adobeXd',
+      }, {
+          logo: 'gimp',
+          // }, {
+          //   logo: 'inkScape',
+      }];
 
-    this.logoProject = [{
-      logo: 'github',
-    }, {
-      logo: 'gitlab',
-    }, {
-      logo: 'docker',
-    }, {
-      logo: 'mailchimp',
-    }, {
-      logo: 'googleAnalitics',
-    }, {
-      logo: 'libreoffice',
-    }];
+      this.logoProject = [{
+          logo: 'github',
+      }, {
+          logo: 'gitlab',
+      }, {
+          logo: 'docker',
+      }, {
+          logo: 'mailchimp',
+      }, {
+          logo: 'googleAnalitics',
+      }, {
+          logo: 'libreoffice',
+      }, {
+          logo: 'office',
+      }, {
+          logo: 'doc',
+      }];
 
-    this.project = [{
-      photo: 'agriRescue.png',
-      title: 'Agri-Rescue',
-      text: ' Création du site web pour Agri-Rescue.',
-      // hrefImg: 'http://www.Agri-Rescue.com',
-      // hrefGit: 'http://www.composites-technologies.com/',
-      // hrefGlobe: 'http://www.Agri-Rescue.com',
-      globe: 'globe'
-    }, {
-      photo: 'cett.png',
-      title: 'Supervision',
-      text: ' Création d\'une supervision pour composite & Technologie.',
-      hrefImg: 'http://www.composites-technologies.com/',
-      hrefGit: 'http://www.composites-technologies.com/',
-      hrefGlobe: 'http://www.composites-technologies.com/',
-      globe: 'globe'
-    }, {
-      photo: 'vdf.png',
-      title: 'Vrai du Faux',
-      text: ' Création d’une application web pour le Bureau d’Information de Jeunesse',
-      hrefImg: 'http://vps366016.ovh.net/#/login',
-      hrefGit: 'https://github.com/WildCodeSchool/laloupe-0916-bij',
-      git: 'github',
-      hrefGlobe: 'http://vps366016.ovh.net/#/login',
-      globe: 'globe'
-    }, {
-      photo: 'chlaloupe.png',
-      title: 'Centre Hospitalier',
-      text: 'Création d\'un site web pour l\'hôpital de La Loupe.',
-      hrefImg: 'http://hopital-nogent.wildcodeschool.fr/#/',
-      hrefGit: 'https://github.com/WildCodeSchool/laloupe-0916-ch-laloupe',
-      git: 'github',
-      hrefGlobe: 'http://hopital-nogent.wildcodeschool.fr/#/',
-      globe: 'globe'
-    }];
+      this.project = [{
+          photo: 'agriRescue.png',
+          title: 'Agri-Rescue',
+          text: ' Création du site web pour Agri-Rescue.',
+          // hrefImg: 'http://www.Agri-Rescue.com',
+          // hrefGit: 'http://www.composites-technologies.com/',
+          // hrefGlobe: 'http://www.Agri-Rescue.com',
+          // globe: 'globe'
+      }, {
+          photo: 'cett.png',
+          title: 'Supervision',
+          text: ' Création d\'une supervision pour composite & Technologie.',
+          hrefImg: 'http://www.composites-technologies.com/',
+          hrefGit: 'http://www.composites-technologies.com/',
+          hrefGlobe: 'http://www.composites-technologies.com/',
+          globe: 'globe'
+      }, {
+          photo: 'vdf.png',
+          title: 'Vrai du Faux',
+          text: ' Création d’une application web pour le Bureau d’Information de Jeunesse',
+          hrefImg: 'http://vps366016.ovh.net/#/login',
+          hrefGit: 'https://github.com/WildCodeSchool/laloupe-0916-bij',
+          git: 'github',
+          hrefGlobe: 'http://vps366016.ovh.net/#/login',
+          globe: 'globe'
+      }, {
+          photo: 'chlaloupe.png',
+          title: 'Centre Hospitalier',
+          text: 'Création d\'un site web pour l\'hôpital de La Loupe.',
+          hrefImg: 'http://hopital-nogent.wildcodeschool.fr/#/',
+          hrefGit: 'https://github.com/WildCodeSchool/laloupe-0916-ch-laloupe',
+          git: 'github',
+          hrefGlobe: 'http://hopital-nogent.wildcodeschool.fr/#/',
+          globe: 'globe'
+      }];
 
-    this.Hackathon = [{
-      photo: 'agreenstartup.png',
-      title: 'Agrivideo',
-      trophy: 'trophy',
-      text: '2eme - Hackathon Agreen StartUp 2016 - 48H',
-      hrefImg: 'https://github.com/Celine-Langlade/Agrivideo',
-      git: 'github',
-      hrefGit: 'https://github.com/Celine-Langlade/Agrivideo',
-    }, {
-      photo: 'wcs.png',
-      title: 'Movember',
-      trophy: 'trophy',
-      text: '1er - Hackathon Wild Codes School - 31H',
-      hrefImg: 'https://github.com/Jipsian/Hackathon-Movember',
-      git: 'github',
-      hrefGit: 'https://github.com/Jipsian/Hackathon-Movember',
-    }, {
-      photo: 'urotech.png',
-      title: 'Uro-Tech',
-      trophy: 'trophy',
-      text: 'Prix du jury - Hackathon Uro-Tech Challenge - 24H',
-      hrefImg: 'https://github.com/stephsen/Hackathon_urotech',
-      git: 'github',
-      hrefGit: 'https://github.com/stephsen/Hackathon_urotech',
-    }, {
-      photo: 'airliquide.png',
-      title: 'Air liquide',
-      trophy: 'trophy',
-      text: '1er - Hackathon Airliquid - 24H',
-      hrefImg: 'https://github.com/Jipsian/hackathon-b-come',
-      git: 'github',
-      hrefGit: 'https://github.com/Jipsian/hackathon-b-come',
-    }];
-    this.fun = [{
-      photo: 'repereage.png',
-      title: 'Repère-Age',
-      text: ' Album Photo',
-      hrefImg: 'https://flic.kr/s/aHskBXxZtA',
-      hrefGlobe: 'https://flic.kr/s/aHskBXxZtA',
-      globe: 'globe',
-    }, {
-      photo: 'blog.png',
-      title: 'CamJr Blog',
-      text: ' Blog',
-      hrefImg: 'https://camjrweb.wordpress.com/',
-      hrefGlobe: 'https://camjrweb.wordpress.com/',
-      globe: 'globe',
-    }, {
-      photo: 'perrette.jpg',
-      title: 'Perrette',
-      text: 'Site web responsive',
-      hrefImg: 'https://camjr.github.io/perrette-et-ses-vaches/',
-      hrefGit: 'https://github.com/Jipsian/Projet-Perrette-et-ses-vaches',
-      hrefGlobe: 'https://camjr.github.io/perrette-et-ses-vaches/',
-      git: 'github',
-      globe: 'globe',
-    }, {
-      photo: 'lotus.png',
-      title: 'Lotus X mjml',
-      text: ' Light is right',
-      hrefImg: 'https://github.com/CamJr/lotusXmjml/',
-      hrefGit: 'https://github.com/CamJr/lotusXmjml/',
-      hrefGlobe: 'https://camjr.github.io/lotusXmjml/',
-      git: 'github',
-      globe: 'globe',
-    }, {
-      photo: 'nopsis.png',
-      title: 'Nopsis',
-      text: ' Prochainement',
-      // hrefImg: 'http://hopital-nogent.wildcodeschool.fr/#/',
-      // hrefGit: 'https://fr.linkedin.com/in/',
-      // git: 'github'
-    }];
+      this.Hackathon = [{
+          photo: 'agreenstartup.png',
+          title: 'Agrivideo',
+          trophy: 'trophy',
+          text: '2eme - Hackathon Agreen StartUp 2016 - 48H',
+          hrefImg: 'https://github.com/Celine-Langlade/Agrivideo',
+          git: 'github',
+          hrefGit: 'https://github.com/Celine-Langlade/Agrivideo',
+      }, {
+          photo: 'wcs.png',
+          title: 'Movember',
+          trophy: 'trophy',
+          text: '1er - Hackathon Wild Codes School - 31H',
+          hrefImg: 'https://github.com/Jipsian/Hackathon-Movember',
+          git: 'github',
+          hrefGit: 'https://github.com/Jipsian/Hackathon-Movember',
+      }, {
+          photo: 'urotech.png',
+          title: 'Uro-Tech',
+          trophy: 'trophy',
+          text: 'Prix du jury - Hackathon Uro-Tech Challenge - 24H',
+          hrefImg: 'https://github.com/stephsen/Hackathon_urotech',
+          git: 'github',
+          hrefGit: 'https://github.com/stephsen/Hackathon_urotech',
+      }, {
+          photo: 'airliquide.png',
+          title: 'Air liquide',
+          trophy: 'trophy',
+          text: '1er - Hackathon Airliquid - 24H',
+          hrefImg: 'https://github.com/Jipsian/hackathon-b-come',
+          git: 'github',
+          hrefGit: 'https://github.com/Jipsian/hackathon-b-come',
+      }];
+      this.fun = [{
+          photo: 'repereage.png',
+          title: 'Repère-Age',
+          text: ' Album Photo',
+          hrefImg: 'https://flic.kr/s/aHskBXxZtA',
+          hrefGlobe: 'https://flic.kr/s/aHskBXxZtA',
+          globe: 'globe',
+      }, {
+          photo: 'blog.png',
+          title: 'CamJr Blog',
+          text: ' Blog',
+          hrefImg: 'https://camjrweb.wordpress.com/',
+          hrefGlobe: 'https://camjrweb.wordpress.com/',
+          globe: 'globe',
+      }, {
+          photo: 'perrette.jpg',
+          title: 'Perrette',
+          text: 'Site web responsive',
+          hrefImg: 'https://camjr.github.io/perrette-et-ses-vaches/',
+          hrefGit: 'https://github.com/Jipsian/Projet-Perrette-et-ses-vaches',
+          hrefGlobe: 'https://camjr.github.io/perrette-et-ses-vaches/',
+          git: 'github',
+          globe: 'globe',
+      }, {
+          photo: 'lotus.png',
+          title: 'Lotus X mjml',
+          text: ' Light is right',
+          hrefImg: 'https://github.com/CamJr/lotusXmjml/',
+          hrefGit: 'https://github.com/CamJr/lotusXmjml/',
+          hrefGlobe: 'https://camjr.github.io/lotusXmjml/',
+          git: 'github',
+          globe: 'globe',
+      // }, {
+      //     photo: 'nopsis.png',
+      //     title: 'Nopsis',
+      //     text: ' Prochainement',
+          // hrefImg: 'http://hopital-nogent.wildcodeschool.fr/#/',
+          // hrefGit: 'https://fr.linkedin.com/in/',
+          // git: 'github'
+      }];
 
-    this.contact = [{
-      href: 'mailto:cfontainef1@gmail.com',
-      logo: 'envelope',
-      text: 'Mail',
-    }, {
-      href: 'https://www.google.fr/maps/place/28200+Ch%C3%A2teaudun/@48.0718202,1.2506053,12z/data=!3m1!4b1!4m5!3m4!1s0x47e37f89cb145b09:0x40dc8d7053984a0!8m2!3d48.070369!4d1.327696?sa=X&ved=0ahUKEwjiu_ikyaPTAhUCuRoKHbSpCi4Q8gEIjwEwEQ',
-      logo: 'map-marker',
-      text: 'Adresse',
+      this.contact = [{
+          href: 'mailto:cfontainef1@gmail.com',
+          logo: 'envelope',
+          text: 'Mail',
+      }, {
+          href: 'https://www.google.fr/maps/place/28200+Ch%C3%A2teaudun/@48.0718202,1.2506053,12z/data=!3m1!4b1!4m5!3m4!1s0x47e37f89cb145b09:0x40dc8d7053984a0!8m2!3d48.070369!4d1.327696?sa=X&ved=0ahUKEwjiu_ikyaPTAhUCuRoKHbSpCi4Q8gEIjwEwEQ',
+          logo: 'map-marker',
+          text: 'Adresse',
 
-    }, {
-      href: 'tel:06 23 64 73 96',
-      logo: 'phone',
-      text: 'Téléphone',
+      }, {
+          href: 'tel:06 23 64 73 96',
+          logo: 'phone',
+          text: 'Téléphone',
 
-    }];
+      }];
 
   }
